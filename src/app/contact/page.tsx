a569561b6580629856b657a796e342aa462ac4e5
Akshansh Kakkar <akshanshkakkar92@gmail.com>
2026-06-02 15:07:07 +0530

    done with adding about and home page and all set

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,28 @@
+import { BrushCleaning } from "lucide-react";
+import { Geist } from "next/font/google";
+import Link from "next/link";
+const geist = Geist({
+    subsets : ["latin"]
+})
+export default function page() {
+  return (
+    <div className="flex justify-center items-center h-[90vh]">
+      <div className="bg-white p-12 flex gap-3 flex-col justify-center items-center p-12 border-2 rounded-xl">
+        <div className="text-2xl">
+          <BrushCleaning size={64} />
+        </div>
+        <div className={`text-2xl text-[#00697A] font-bold ${geist.className}`}>
+          This is a dummy Page. Click{" "}
+          <Link
+            href={"https://github.com/akshansh-kakkkar/"}
+            target="_blank"
+            rel="noopener norefferer"
+            className="underline"
+          >
+            here
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
